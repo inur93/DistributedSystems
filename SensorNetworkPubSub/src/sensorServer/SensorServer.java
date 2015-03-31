8ecd9b289cd6652fbdbd8cc4025d4a919e30461d
Runi <Runi@192.168.10.101>
2015-03-31 17:59:53 +0200

    work in progress

--- SensorNetworkPubSub/src/sensorServer/SensorServer.java
+++ SensorNetworkPubSub/src/sensorServer/SensorServer.java
@@ -1,5 +1,6 @@
 package sensorServer;
 import java.net.*;
+import java.util.Enumeration;
 import java.io.*;
 
 import common.PropertyHelper;
@@ -7,81 +8,134 @@ import common.PropertyHelper;
 
 public class SensorServer implements Runnable{
 	private DatagramSocket socket;
+	private static final String DEFAULT_NAME = "255.255.255.255";
+	private static final int DEFAULT_PORT = 8888;
+	
 	public static final String FILE_NAME = "temperature";
 	public static final int DATA_SIZE = 5;
 	public static void main(String[] args){ 
+		Thread discoveryThread = new Thread(DiscoveryThread.getInstance());
+		discoveryThread.start();
 		SensorServer s = new SensorServer();
 		s.run();
 	}
 
 	public void run(){
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		ServerSocket sensorSocket = null;
-		Socket clientSocket = null;
-		DataInputStream inputStream = null;
 		try{
-			sensorSocket = new ServerSocket(9999);
-			System.out.println("Socket ready");
-		} catch(IOException e){
-			System.err.println("failed to create socket: " + e.getMessage());
+		socket = new DatagramSocket();
+		socket.setBroadcast(true);
+		byte[] data = "TEST_SENDING".getBytes();
+		try{
+			DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(DEFAULT_NAME), DEFAULT_PORT);
+			socket.send(packet);
+			System.out.println(getClass().getName() + " >> Request sent to: " + DEFAULT_NAME);
+		}catch(Exception e){
+			System.err.println("failed to send packet");
 		}
-		int index = PropertyHelper.findLastIndex();
-		while(true){
-			try{
-				clientSocket = sensorSocket.accept();			
-				System.out.println("client connected");
-				inputStream = new DataInputStream(clientSocket.getInputStream());
-
-				char in;
-				int charNo = 0;
-				char[] temp = new char[DATA_SIZE];
-				// waiting for data
-				while((in = (char) inputStream.readByte()) == '_'){}
-				do{
-					// if trying to write data longer than 5 chars at a time
-					// or if data starting with ',' or '.' breaks and runs try clause again
-					if(charNo >= DATA_SIZE){
-						System.err.println("wrong data format received");
-						break; // goes outside whole try catch statement and closes client connection
-					}
-					temp[charNo] = in;
-					charNo++;
-				}while((in = (char) inputStream.readByte()) != '_');
-
-				// writing temperature measurement to file 
-				if(writeToProperty(String.valueOf(index), temp)){
-					index++;	
+		
+		Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
+		while(interfaces.hasMoreElements()){
+			NetworkInterface networkInterface = (NetworkInterface) interfaces.nextElement();
+			
+			if(networkInterface.isLoopback() || !networkInterface.isUp()){
+				continue;
+			}
+			
+			for(InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()){
+				InetAddress broadcast = interfaceAddress.getBroadcast();
+				if(broadcast == null){
+					continue;
 				}
-
-
-			}catch(IOException e){
-				System.err.println("failed to connect to client: " + e.getMessage());
-			}
-
-			try {
-				if(!clientSocket.isConnected()){
-					clientSocket.close();	
+				
+				try{
+					DatagramPacket packet = new DatagramPacket(data, data.length, broadcast, DEFAULT_PORT);
+					socket.send(packet);
+				}catch(Exception e){
+					System.err.println("failed to broadcast packet");
 				}
-			} catch (IOException e) {
-				System.err.println("could not close client connection: " + e.getMessage());
-				break;
+				
+				System.out.println(getClass().getName() + " >> Request sent to: " + broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
+				
 			}
-		}	
-		try {
-			if(!sensorSocket.isClosed()){
-				sensorSocket.close();
-			}
-		} catch (IOException e) {
-			System.err.println("serversocket failed to close: " + e.getMessage());
 		}
+		
+		System.out.println(getClass().getName() + ">>> Done looping over all network interfaces. Now waiting for a reply!");
+
+		byte[] receiveBuffer = new byte[15000];
+		DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+		socket.receive(receivePacket);
+		
+		System.out.println(getClass().getName() + ">> Broadcast response from server: " + receivePacket.getAddress().getHostAddress());
+		
+		String msg = new String(receivePacket.getData()).trim();
+		if(msg.equals("TEST_RECEIVE")){
+			System.out.println("server ip address to store: " + receivePacket.getAddress());
+		}
+		
+		socket.close();
+	}catch(IOException e){
+		
+	}
+		
+//		ServerSocket sensorSocket = null;
+//		Socket clientSocket = null;
+//		DataInputStream inputStream = null;
+//		try{
+//			sensorSocket = new ServerSocket(9999);
+//			System.out.println("Socket ready");
+//		} catch(IOException e){
+//			System.err.println("failed to create socket: " + e.getMessage());
+//		}
+//		int index = PropertyHelper.findLastIndex();
+//		while(true){
+//			try{
+//				clientSocket = sensorSocket.accept();			
+//				System.out.println("client connected");
+//				inputStream = new DataInputStream(clientSocket.getInputStream());
+//
+//				char in;
+//				int charNo = 0;
+//				char[] temp = new char[DATA_SIZE];
+//				// waiting for data
+//				while((in = (char) inputStream.readByte()) == '_'){}
+//				do{
+//					// if trying to write data longer than 5 chars at a time
+//					// or if data starting with ',' or '.' breaks and runs try clause again
+//					if(charNo >= DATA_SIZE){
+//						System.err.println("wrong data format received");
+//						break; // goes outside whole try catch statement and closes client connection
+//					}
+//					temp[charNo] = in;
+//					charNo++;
+//				}while((in = (char) inputStream.readByte()) != '_');
+//
+//				// writing temperature measurement to file 
+//				if(writeToProperty(String.valueOf(index), temp)){
+//					index++;	
+//				}
+//
+//
+//			}catch(IOException e){
+//				System.err.println("failed to connect to client: " + e.getMessage());
+//			}
+//
+//			try {
+//				if(!clientSocket.isConnected()){
+//					clientSocket.close();	
+//				}
+//			} catch (IOException e) {
+//				System.err.println("could not close client connection: " + e.getMessage());
+//				break;
+//			}
+//		}	
+//		try {
+//			if(!sensorSocket.isClosed()){
+//				sensorSocket.close();
+//			}
+//		} catch (IOException e) {
+//			System.err.println("serversocket failed to close: " + e.getMessage());
+//		}
 	}
 
 	/**
