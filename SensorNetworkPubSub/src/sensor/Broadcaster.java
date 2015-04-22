beaa0ee323176a9c83ba4e70489ab619000a1d4a
Runi <Runi@Runi-laptop>
2015-04-22 22:37:55 +0200

    minor update

--- SensorNetworkPubSub/src/sensor/Broadcaster.java
+++ SensorNetworkPubSub/src/sensor/Broadcaster.java
@@ -27,53 +27,49 @@ public class Broadcaster implements Runnable{
 
 
 			packet = new DatagramPacket(data, data.length, InetAddress.getByName(SensorController.DEFAULT_NAME), SensorController.PACKET_PORT);
-			socket.send(packet);
-			controller.addMsgToLog(getClass().getSimpleName() + ">> data sent: " + new String(packet.getData()));
-			System.out.println(getClass().getName() + ">> broadcasting sensor is ready to: " + SensorController.DEFAULT_NAME);
+//			socket.send(packet);
+//			controller.addMsgToLog(getClass().getSimpleName() + ">> data sent: " + new String(packet.getData()));
 
 		}catch(IOException e){
 			System.err.println(getClass().getName() + ">> broadcast packet failed");
 		}	
-
-		// Broadcast the message over all the network interfaces
-
 		Enumeration<NetworkInterface> interfaces = null;
 		try {
 			interfaces = NetworkInterface.getNetworkInterfaces();
-
-
+	
+	
 			while (interfaces.hasMoreElements()) {
-
+	
 				NetworkInterface networkInterface = interfaces.nextElement();	 
-
+	
 				if (networkInterface.isLoopback() || !networkInterface.isUp()) {
-
+	
 					continue; // Don't want to broadcast to the loopback interface
-
+	
 				}
-
+	
 				for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
-
+	
 					InetAddress broadcast = interfaceAddress.getBroadcast();
-
+	
 					if (broadcast == null) {
 						continue;
 					}
-
+	
 					// Send the broadcast package!
-
+	
 					try {
-
+	
 						socket.send(packet);
 						controller.addMsgToLog(getClass().getSimpleName() + ">> sent data: " + new String(packet.getData()));
 					} catch (IOException e) {
 						System.err.println(getClass().getName() + ">> failed to send packet");
 					}
-
+	
 					System.out.println(getClass().getName() + ">>> Request packet sent to: " + broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
-
+	
 				}
-
+	
 			}
 		} catch (SocketException e1) {
 			// TODO Auto-generated catch block
