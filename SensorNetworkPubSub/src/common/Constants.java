3a320737e74802f32c9e116d190b0eb842d640bd
Runi <Runi@192.168.10.101>
2015-04-23 11:32:40 +0200

    update

--- SensorNetworkPubSub/src/common/Constants.java
+++ SensorNetworkPubSub/src/common/Constants.java
@@ -3,9 +3,10 @@ package common;
 public class Constants {
 
 	public static final String DEFAULT_NAME =  "255.255.255.255"; //"192.168.10.255";//
+	
 	public static final int PUBLISHER_PORT = 8889;
-//	public static final int PACKET_PORT = 8888;
-
+	public static final int SUBSCRBER_PORT = 8888;
+	
 	public static final String FILE_NAME = "temperature";
 	
 	public static enum Topics{TEMP, LIGHT};
@@ -15,11 +16,6 @@ public class Constants {
 	public static final int PACKET_SIZE = 512;
 	public static final int DATA_SIZE = 5;
 	
-	
-//	
-//	public static final int PACKET_PORT = 8889;
-//	public static final int RECEIVER_PORT = 8888;
-	public static final int SUBSCRBER_PORT = 8888;
 	
 	
 	
